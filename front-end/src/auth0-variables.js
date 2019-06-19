e427f4e8f5283c627258e884a000d3152d1871ca
Levi-Huynh <Levi_huynh@yahoo.com>
2019-06-19 12:19:12 -0700

    roles working for admin and users

---
+++ front-end/src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "dev2911.auth0.com",
+    roleUrl: "https://front-end/role",
+    clientId: "TKNdGO64rqRJSDBdJh7WIhhjvYF60rdN",
+    callbackUrl: "http://localhost:3000/callback"
+  };
---
+++ front-end/src/posts.js
@@ -0,0 +1,24 @@
+const posts = [
+    {
+      id: "a",
+      title: "Post a",
+      ownerId: "auth0|5cfdda062e84a30daff1ca5d"
+    },
+    {
+      id: "b",
+      title: "Post b",
+      ownerId: ""
+    },
+    {
+      id: "c",
+      title: "Post c",
+      ownerId: "auth0|5cfdda062e84a30daff1ca5d"
+    },
+    {
+      id: "d",
+      title: "Post d",
+      ownerId: "5cfdda062e84a30daff1ca5d"
+    }
+  ];
+  
+  export default posts;
